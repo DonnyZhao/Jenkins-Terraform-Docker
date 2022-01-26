b5fc564ee353e3a453632ad96862162e3e6021b1
DonnyZhao <zhaodongn@hotmail.com>
2022-01-26 16:18:24 +1100

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -1,7 +1,7 @@
 # Configure and downloading plugins for aws
 provider "aws" {
-access_key = "AKIARCIVPQFCB3G3F3WJ"
-secret_key = "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"
+//access_key = "AKIARCIVPQFCB3G3F3WJ"
+//secret_key = "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"
 region = "ap-southeast-2"
 }
 
